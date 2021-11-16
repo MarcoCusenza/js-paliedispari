9759d17d77a5beec144840ef5774e59d038c22e6
Marco <marco.cusenza.me@gmail.com>
2021-11-16 14:51:30 +0100

    isPalindroma

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 const result = document.getElementById("result");
 
 //PALINDROMA
+const string = prompt("Inserisci una parola");
+
+if (isPalindroma(string) == false) {
+    result.innerHTML = "La parola inserita NON è palindroma";
+} else {
+    result.innerHTML = "La parola inserita è palindroma";
+}
+
+function isPalindroma(string) {
+    for (let i = 0; i < (string.length / 2); i++) {
+        if (string[i] != string[string.length - (i + 1)]) {
+            return false;
+        }
+    }
+    return true;
+}
 
 
 //PARI O DISPARI
